046752dada7d7dad2f1598f1443bcc61db8f65cc
songsuyoung <81684148+songsuyoung@users.noreply.github.com>
2021-09-10 16:56:45 +0900

    Rename ex5.java to ex5(실습문제12-2).java

---
+++ ex5(실습문제12-2).java
@@ -0,0 +1,37 @@
+package example;
+import java.util.*;
+public class ex5 {
+
+	
+	public static void main(String[] args) {
+		
+		int num1;
+		int num2;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("연산>>");
+		String line = scan.nextLine(); //라인으로 입력 받아서 split 내장함수를 이용해서 " "을 중심으로 자른다.
+		String tmp[]=line.split(" ");
+		
+		num1=Integer.parseInt(tmp[0]); //잘랐을때 나오는 3등분 중 가운데 값을 op이라고 생각하여 작성하였다.
+		num2=Integer.parseInt(tmp[2]);
+		
+		switch(tmp[1]) {
+		case "+":
+			System.out.println(num1+"+"+num2+"의 계산결과는 "+(num1+num2));
+			break;
+		case "-":
+			System.out.println(num1+"-"+num2+"의 계산결과는 "+(num1-num2));
+			break;
+		case "*":
+			System.out.println(num1+"*"+num2+"의 계산결과는 "+(num1*num2));
+			break;
+		case "/":
+			if(num2!=0)
+				System.out.println(num1+"/"+num2+"의 계산결과는 "+(num1/num2));
+			else
+				System.out.println("0으로 나눌수 없습니다.");
+			break;
+		}
+	}
+}
